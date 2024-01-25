1810ed21354dddb92aebb6533491f9bead0d390d
Aakash <aakash3.mahajan@gmail.com>
2024-01-25 06:58:32 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 from flask import Flask, render_template, url_for,request
+import numpy as np
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
 
-@app.route('/', methods=['POST','GET'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    result = None
 
-@app. route("/predict", methods=['POST'])
-def predict():
-    profilepic = int(request.form ['profilepic'])
-    username = int(request.form ['username'])
-    words = int(request.form ['words'])
-    fullname = int(request.form ['fullname'])
-    equal = int(request.form ['equal'])
-    description = int(request.form ['description'])
-    external = int(request.form ['external'])
-    private = int(request.form ['private'])
-    posts = int(request.form ['posts'])
-    followers = int(request.form ['followers'])
-    following = int(request.form ['following'])
-    
-    prediction = model.predict([[profilepic, username, words, fullname, equal, description, external, private, posts, followers, following]])
-    output = round(prediction[0])
-    return render_template('index.html',prediction_text=f'The account is {output}')
+    if request.method == "POST":
+        # Get form data
+        profilepic = float(request.form ['profilepic'])
+        username = float(request.form ['username'])
+        words = float(request.form ['words'])
+        fullname = float(request.form ['fullname'])
+        equal = float(request.form ['equal'])
+        description = float(request.form ['description'])
+        external = float(request.form ['external'])
+        private = float(request.form ['private'])
+        posts = float(request.form ['posts'])
+        followers = float(request.form ['followers'])
+        following = float(request.form ['following'])
+        
+        print(request.form)
+        
+        features = [profilepic, username, words, fullname, equal, description, external, private, posts, followers, following]
 
+        # Make prediction
+        prediction = model.predict([features])
+        
+        if prediction[0] == 1:
+            result = "Fake Profile"            
+        else:
+            result = "Real Profile"
+            
+        print("Features:", features)  # Check the features being passed to the model
+        print("Prediction:", prediction)
+
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
